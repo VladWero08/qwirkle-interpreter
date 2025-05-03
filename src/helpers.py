757592d064bb60a279c854c6873d6b16800cd888
VladWero08 <vlad_wero@yahoo.com>
2025-05-03 14:52:40 +0300

    added functions to detect the type of tile by template matching

--- src/helpers.py
+++ src/helpers.py
@@ -1,3 +1,4 @@
+import os
 import cv2 as cv
 import numpy as np
 
@@ -6,3 +7,11 @@ def show_image(image: np.ndarray, name: str = "image") -> None:
     cv.imshow(name, image)
     cv.waitKey(0)
     cv.destroyAllWindows()
+
+def read_template(path: list[str]) -> np.ndarray | None:
+    template_path = os.path.join(*path)
+    template_path = os.path.normpath(template_path)
+    template = cv.imread(template_path)
+    template = cv.resize(template, (100, 100))
+
+    return template
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,17 @@
 import cv2 as cv
 import numpy as np
 
+from src.helpers import show_image
+
+def auto_canny(image, sigma=0.5):
+    v = np.median(image)
+    
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+
+    return cv.Canny(image, lower, upper)
+
+
 def board_preprocess(image: np.ndarray) -> np.ndarray:
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     blurred = cv.medianBlur(gray, 11)
@@ -12,3 +23,12 @@ def board_preprocess(image: np.ndarray) -> np.ndarray:
     closed = cv.morphologyEx(threshold, cv.MORPH_CLOSE, kernel)
 
     return closed
+
+
+def tile_preprocess(tile: np.ndarray) -> np.ndarray:
+    tile = cv.cvtColor(tile, cv.COLOR_BGR2GRAY)
+    tile = cv.medianBlur(tile, 11)
+    _, tile = cv.threshold(tile, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    # tile = auto_canny(tile)
+
+    return tile
