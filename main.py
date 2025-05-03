4dbeb10a2f9a1ee5375c936663d0fd8dd0b2a81d
VladWero08 <vlad_wero@yahoo.com>
2025-05-03 14:53:29 +0300

    removed the board templates

--- main.py
+++ main.py
@@ -7,5 +7,4 @@ from src.tiles_extraction import get_patches
 if __name__ == "__main__":
     image = cv.imread("./playground/board-perspective.jpg")
     board = get_board(image)
-    show_image(board)
     patches = get_patches(board)
